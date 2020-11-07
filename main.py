8c6c5b7fc9d9629b37b0661b5880420721246a8c
yoshiwo <yoshiwo115@gmail.com>
2020-11-07 13:49:08 +0900

    {helloworld_change}

--- main.py
+++ main.py
@@ -51,10 +51,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage) #Messageが来たとき、start()追加された時などもある
 def handle_message(event):
     reply_text = event.message.text #コメントを抜き出す
-	if "好き" in event.message.text:
-		reply_text= "私もよ"
-	else:
-		reply_text= event.message.text
+	
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text) #Messageが来たとき、これを送る
